bfd39fe8f18e532ffd2a2ec6c4cd7f0dad563458
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-06-07 01:17:18 +0300

    additon of ovride

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -4,16 +4,16 @@ import java.util.Objects;
 
 public class Squad {
     @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Squad squad = (Squad) o;
-//        return maxNum == squad.maxNum &&
-//                id == squad.id &&
-//                Objects.equals(squadName, squad.squadName) &&
-//                Objects.equals(cause, squad.cause);
-//    }
-//
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Squad squad = (Squad) o;
+        return maxNum == squad.maxNum &&
+                id == squad.id &&
+                Objects.equals(squadName, squad.squadName) &&
+                Objects.equals(cause, squad.cause);
+    }
+
 //    @Override
 //    public int hashCode() {
 //        return Objects.hash(squadName, maxNum, cause, id);
