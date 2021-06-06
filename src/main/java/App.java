2f38a24080e3bc4a629ef34d49ce262206196ef8
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-06-07 00:57:53 +0300

    App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,23 +1,23 @@
-//
-//import models.Hero;
-//import models.Squad;
-//
-//import spark.ModelAndView;
-//import spark.template.handlebars.HandlebarsTemplateEngine;
-//
-//import java.util.ArrayList;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import static spark.Spark.*;
-//public class App {
-//    public static void main(String[] args) {
-//        ProcessBuilder process = new ProcessBuilder();
-//        Integer port;
-//
-//
-//
+
+import models.Hero;
+import models.Squad;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+public class App {
+    public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+
+
 //        if (process.environment().get("PORT") != null) {
 //            port = Integer.parseInt(process.environment().get("PORT"));
 //        } else {
